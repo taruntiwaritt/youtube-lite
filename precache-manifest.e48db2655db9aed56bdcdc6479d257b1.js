51dd90acc80f34a716b23a29f1d36193f751cb98
tarun tiwari <git config --global user.email taruntiwaritt1997@gmail.com>
2020-07-11 16:23:57 +0530

    Updates

---
+++ precache-manifest.e48db2655db9aed56bdcdc6479d257b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ecf4fbd1a1993574a591d322c496fa",
+    "url": "/youtube-lite/index.html"
+  },
+  {
+    "revision": "a4a3980150ac08e8b59e",
+    "url": "/youtube-lite/static/css/main.f69a66d2.chunk.css"
+  },
+  {
+    "revision": "35e5d30d56b02a9f4bad",
+    "url": "/youtube-lite/static/js/2.2605bfb9.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtube-lite/static/js/2.2605bfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a3980150ac08e8b59e",
+    "url": "/youtube-lite/static/js/main.84c08229.chunk.js"
+  },
+  {
+    "revision": "e02e709666d4ebe866be",
+    "url": "/youtube-lite/static/js/runtime-main.cdc194c7.js"
+  }
+]);
